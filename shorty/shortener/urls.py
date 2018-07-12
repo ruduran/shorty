26e6fec9d960850c0e658d3ad2e722179a2f052c
Rubén Durán <ruduran@users.noreply.github.com>
2018-07-12 13:27:33 +0200

    Improve the code generation
    * Add the date to include some randomness
    * Handle possible collisions

--- shorty/shortener/urls.py
+++ shorty/shortener/urls.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('(?P<code>[0-9a-zA-Z]+)', views.redirection, name='redirection'),
+    re_path('(?P<code>[0-9a-zA-Z\+_]+)', views.redirection, name='redirection'),
     re_path('', views.index, name='index'),
 ]
--- shorty/shortener/utils.py
+++ shorty/shortener/utils.py
@@ -1,12 +1,26 @@
 import hashlib
 import base64
+import datetime
+
+from django.db.utils import IntegrityError
 
 from .models import ShortenedUrl
 
 
 def generate_and_get_url_code(url):
-    digest = hashlib.sha1(url.encode()).digest()
+    now = datetime.datetime.now()
+    str_to_hash = '{}{}'.format(url, now.isoformat())
+    digest = hashlib.sha1(str_to_hash.encode()).digest()
     code = base64.b64encode(digest)
-    short_url = ShortenedUrl(url=url, code=code[:3].decode())
-    short_url.save()
+
+    saved = False
+    length = 3
+    while not saved:
+        try:
+            short_url = ShortenedUrl(url=url, code=code[:length].decode().replace('/', '_'))
+            short_url.save()
+            saved = True
+        except IntegrityError:
+            length += 1
+
     return short_url.code
