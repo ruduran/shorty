7667a5d86ca829eb23231b2aec47d8a2a0f7ad09
Rubén Durán <ruduran@users.noreply.github.com>
2018-07-12 13:29:29 +0200

    Add validation to make sure the protocol is included

--- shorty/shortener/views.py
+++ shorty/shortener/views.py
@@ -6,16 +6,26 @@ from .models import ShortenedUrl
 from .utils import generate_and_get_url_code
 
 
+def validate(url):
+    if '://' not in url:
+        return "You need to enter the protocol. For instance http://{}".format(url)
+    else:
+        return ""
+
+
 def index(request):
     context = {}
     if request.POST.get('url'):
         url = request.POST['url']
-        code = generate_and_get_url_code(url)
+        context['url'] = url
 
-        path = reverse('redirection', args=[code])
-
-        context['url'] = request.POST['url']
-        context['short_url'] = "http://{}{}".format(request.get_host(), path)
+        validation_error = validate(url)
+        if validation_error:
+            context['error'] = validation_error
+        else:
+            code = generate_and_get_url_code(url)
+            path = reverse('redirection', args=[code])
+            context['short_url'] = "http://{}{}".format(request.get_host(), path)
 
     return render(request, 'shortener/index.html', context)
 
